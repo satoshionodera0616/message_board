3b51e4e3a5f90c0da33243885c2728f5774b6cd3
satoshi.onodera <ousyo@MyComputer>
2021-09-20 16:42:15 +0900

    Index/NewServletの修正

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,9 +1,8 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
-import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Message;
-import utils.DBUtil;
 
 @WebServlet("/new")
 public class NewServlet extends HttpServlet {
@@ -22,7 +20,17 @@ public class NewServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        EntityManager em = DBUtil.createEntityManager();
+
+        // CSRF対策
+        request.setAttribute("_token",request.getSession().getId());
+
+        // おまじないとしてのインスタンスを生成
+        request.setAttribute("message",new Message());
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
+        rd.forward(request, response);
+
+       /* EntityManager em = DBUtil.createEntityManager();
         em.getTransaction().begin();
 
         // Messageのインスタンスを生成
@@ -45,7 +53,7 @@ public class NewServlet extends HttpServlet {
         em.getTransaction().commit();
 
         // 自動採番されIDの値を表示
-        response.getWriter().append(Integer.valueOf(m.getId()).toString());
+        response.getWriter().append(Integer.valueOf(m.getId()).toString()); */
     }
 
 }
